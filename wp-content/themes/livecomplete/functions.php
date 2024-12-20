934e76c1e82e6014c9236c7b1be818f913928464
warrengalyen <wgalyen@hotmail.com>
2024-12-19 16:21:57 -0800

    Create custom widget for featured products in menu

--- wp-content/themes/livecomplete/functions.php
+++ wp-content/themes/livecomplete/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * LiveComplete functions and definitions
  *
@@ -33,30 +34,155 @@ require get_template_directory() . '/inc/class/class-template-tags.php';
 require get_template_directory() . '/inc/class/class-post-related.php';
 
 /**
-* Functions which enhance the theme by hooking into WordPress.
-*/
+ * Functions which enhance the theme by hooking into WordPress.
+ */
 require get_template_directory() . '/inc/template-functions.php';
 
 /**
-* Load Jetpack compatibility file.
-*/
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
+ * Load Jetpack compatibility file.
+ */
+if (defined('JETPACK__VERSION')) {
+    require get_template_directory() . '/inc/jetpack.php';
 }
 
 /**
-* TGM Plugins
-*/
+ * TGM Plugins
+ */
 require get_template_directory() . '/inc/tgm/recommended-plugins.php';
 
 require get_template_directory() . '/inc/customizer/customizer.php';
 
 
 /**
-* Implement pro features.
-*/
+ * Implement pro features.
+ */
 // require get_template_directory() . '/inc/admin/admin-page.php';
 
-if ( class_exists( 'WooCommerce' ) ) {
+if (class_exists('WooCommerce')) {
     require get_template_directory() . '/inc/woocommerce.php';
 }
+
+class Featured_Product_Widget extends WP_Widget
+{
+    public function __construct()
+    {
+        parent::__construct(
+            'featured_product_widget',
+            'Featured Product Widget',
+            array('description' => 'Displays a WooCommerce product with image, name, description, and shop link')
+        );
+    }
+
+    public function widget($args, $instance)
+    {
+        $product_id = !empty($instance['product_id']) ? $instance['product_id'] : '';
+
+        if ($product_id) {
+            $product = wc_get_product($product_id);
+
+            if ($product) {
+                echo $args['before_widget'];
+?>
+                <div class="featured-product-widget">
+                    <div class="product-image">
+                        <?php echo $product->get_image(); ?>
+                    </div>
+                    <div class="product-content">
+                        <h3 class="product-title"><?php echo $product->get_name(); ?></h3>
+                        <div class="product-description">
+                            <?php echo $product->get_short_description(); ?>
+                        </div>
+                        <a href="<?php echo $product->get_permalink(); ?>" class="shop-now-link">
+                            Shop Now
+                        </a>
+                    </div>
+                </div>
+                <style>
+                    .featured-product-widget {
+                        display: flex;
+                        align-items: center;
+                        gap: 20px;
+                        max-height: 171px;
+                        overflow: hidden;
+                    }
+
+                    .featured-product-widget .product-image {
+                        flex-shrink: 0;
+                        width: 171px;
+                        height: 171px;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                    }
+
+                    .featured-product-widget .product-image img {
+                        max-width: 100%;
+                        max-height: 100%;
+                        width: auto;
+                        height: auto;
+                        object-fit: contain;
+                    }
+
+                    .featured-product-widget .product-content {
+                        flex: 1;
+                        text-align: left;
+                        overflow: hidden;
+                    }
+
+                    .featured-product-widget .product-title {
+                        margin-bottom: 10px;
+                        font-family: var(--text-regular-semi-bold-font-family,
+                                "Roboto-SemiBold",
+                                sans-serif);
+                        font-size: var(--text-regular-semi-bold-font-size, 16px);
+                        line-height: var(--text-regular-semi-bold-line-height, 150%);
+                        font-weight: var(--text-regular-semi-bold-font-weight, 600);
+                    }
+
+                    .featured-product-widget .product-description {
+                        margin-bottom: 15px;
+                        overflow: hidden;
+                        display: -webkit-box;
+                        -webkit-line-clamp: 3;
+                        -webkit-box-orient: vertical;
+                    }
+
+                    .featured-product-widget .shop-now-link {
+                        color: #000;
+                        text-decoration: underline;
+                    }
+                </style>
+        <?php
+                echo $args['after_widget'];
+            }
+        }
+    }
+
+    public function form($instance)
+    {
+        $product_id = !empty($instance['product_id']) ? $instance['product_id'] : '';
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_id('product_id'); ?>">Product ID:</label>
+            <input class="widefat" id="<?php echo $this->get_field_id('product_id'); ?>"
+                name="<?php echo $this->get_field_name('product_id'); ?>"
+                type="number"
+                value="<?php echo esc_attr($product_id); ?>">
+        </p>
+<?php
+    }
+
+    public function update($new_instance, $old_instance)
+    {
+        $instance = array();
+        $instance['product_id'] = (!empty($new_instance['product_id'])) ? strip_tags($new_instance['product_id']) : '';
+        return $instance;
+    }
+}
+
+// Register the widget
+function register_featured_product_widget()
+{
+    register_widget('Featured_Product_Widget');
+}
+add_action('widgets_init', 'register_featured_product_widget');
