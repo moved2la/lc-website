870e1269df9357ee45213d1e358a55b6617dd709
warrengalyen <wgalyen@hotmail.com>
2025-01-14 11:23:52 -0800

    Add wflogs to .gitignore

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1737396193;s:7:"authKey";s:64:"o*&*%{PEXkv^w7~^Zp<opuuP|m&JCn5/LyYE*Qe]{2#mJPr,9oKtH _WB,0ryr=1";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2824;}
