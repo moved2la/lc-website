e14fc7dab46cebcfe2dd11ccade1fcae672d7eb7
warrengalyen <wgalyen@hotmail.com>
2025-01-18 09:44:53 -0800

    Update plugins and .htaccess rules

--- wp-content/plugins/yaymail/assets/admin/js/script.js
+++
@@ -1,2 +0,0 @@
-document.body.style.overflow = "hidden";
-document.body.classList.add("yaymail-customizer-page-builder");
--- wp-content/plugins/yaymail/includes/YayCommerceMenu/assets/js/yaycommerce-menu.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function () {
-	jQuery("#adminmenu li.toplevel_page_yaycommerce ul li > a[href='yaycommerce-help']").attr('href', 'https://yaycommerce.com/support').attr('target', '_blank')
-})
