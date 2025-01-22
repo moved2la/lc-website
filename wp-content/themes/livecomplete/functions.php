2ea29941faf7cbf366393835adfe84e7fca1ab08
warrengalyen <wgalyen@hotmail.com>
2025-01-22 10:27:25 -0800

    Fix WP login page title filter

--- wp-content/themes/livecomplete/functions.php
+++ wp-content/themes/livecomplete/functions.php
@@ -721,7 +721,7 @@ function live_complete_custom_login_logo_title()
 {
     return 'LiveComplete';
 }
-add_filter('login_headertitle', 'live_complete_custom_login_logo_title');
+add_filter('login_headertext', 'live_complete_custom_login_logo_title');
 
 function livecomplete_login_css()
 {
@@ -886,3 +886,24 @@ function mytheme_preview_email()
 }
 
 add_action('wp_ajax_previewemail', 'mytheme_preview_email');
+
+function add_GTM_script_header() {
+    echo "<!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TMTRR55G');</script>
+    <!-- End Google Tag Manager -->";
+}
+add_action('wp_head', 'add_GTM_script_header');
+
+function add_GTM_script_body() {
+    echo '
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TMTRR55G"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        ';
+}
+add_action('wp_body_open', 'add_GTM_script_body');
+
+
